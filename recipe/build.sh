a2e91ff57eb6a10bca45cf87c122ebd9cd60f7d9
imykhailov <imykhailov@anaconda.com>
2026-07-20 13:06:10 +0300

    fix: force autoreconf to regenerate libtool on win, tarball bundles newer autotools than env

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,7 +3,7 @@
 if [[ "$target_platform" == "win-64" ]]; then
   export CFLAGS="$CFLAGS -O3 -Dstrdup=_strdup"
   export ac_cv_have_decl__BitScanForward=yes
-  autoreconf -iv
+  autoreconf -ivf
   ./configure --prefix=$PREFIX --with-int=imath-32 --disable-shared || (cat config.log && false)
   patch_libtool
 else
